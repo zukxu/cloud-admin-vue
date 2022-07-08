a75b4501b8355b9c4e9265975f43e063e5a70bcd
zukxu <zukxu520@163.com>
2022-07-08 17:25:33 +0800

    fix(*): 跨域

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,28 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  css: {
-    preprocessorOptions: {
-      scss: {
-        additionalData: `@import "src/assets/styles/index.scss";`
-      }
+    plugins: [vue()],
+    css: {
+        preprocessorOptions: {
+            scss: {
+                additionalData: `@import "src/assets/styles/index.scss";`
+            }
+        }
+    },
+    server: {
+        open: true,
+        proxy: {
+            '/api/v1/chart': {
+                target: 'http://192.168.12.116:8887',
+                changeOrigin: true
+            },
+            '/api/v1': {
+                target: 'http://192.168.12.116:8888',
+                changeOrigin: true,
+                rewrite: (path) => path.replace('/^\/api/v1', '')
+            }
+        }
     }
-  }
 })
